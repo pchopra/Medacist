6df1896dcffb888ab23bd7dd073c3205f12bce47
Parth Chopra <parthchopra@seas663.wireless-pennnet.upenn.edu>
2016-01-23 14:08:58 -0500

    Adding roles for new users

--- client/accounts/register.js
+++ client/accounts/register.js
@@ -1,5 +1,6 @@
 Template.register.events({
   'submit form': function(event){
+
       event.preventDefault();
       var name = $('[name=name]').val();
       var email = $('[name=email]').val();
@@ -8,13 +9,28 @@ Template.register.events({
 
       Accounts.createUser({
           //username: name,
-          email: email,
-          password: password,
-          profile: {
-            name: name,
-            group: group   
-          }
-      });
+            email: email,
+            password: password,
+            profile: {
+              name: name,
+              group: group   
+            }
+          },  
+          function(err) {
+            if(err) {
+              console.log(err);
+              console.log( $('input[name=type-user1]:checked').val() );
+
+            } else {
+
+              //var type = $('input[name="type"]:checked').val();
+              console.log( $('input[name=type-user1]:checked').val() );
+              Roles.addUsersToRoles(Meteor.userId(), [ $('input[name=type-user1]:checked').val() ]);
+              var bool = Roles.userIsInRole( Meteor.userId(), 'Administrator'); // true
+              console.log("Original: " + Roles.getRolesForUser(Meteor.userId()) );
+              console.log("Result: " + bool);
+            }
+          });
 
       Router.go('/room');
   }
