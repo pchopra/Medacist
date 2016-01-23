aad5f439dd499cbc77cac9a8670438596eec3f40
Marc Tobias <TOBIASM@EMAIL.CHOP.EDU>
2016-01-23 15:20:57 -0500

    add hooks

--- models.js
+++ models.js
@@ -3,3 +3,12 @@
 */
 Messages = new Mongo.Collection('messages');
 Rooms = new Mongo.Collection('rooms');
+
+Meteor.users.after.insert(function (userId, doc) {
+    console.log(doc);
+    if (doc.profile.role === "administrator") {
+        Roles.addUsersToRoles(doc._id, "administrator")
+    } else if (doc.profile.role === "patient") {
+        Roles.addUsersToRoles(doc._id, "patient")
+    }
+});
