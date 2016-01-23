a9151fed6452e12a1c4551fff1b845f9f2dcdbd1
Marc Tobias <TOBIASM@EMAIL.CHOP.EDU>
2016-01-22 22:15:31 -0500

    started routing

--- client/client.js
+++ client/client.js
@@ -1,3 +1,15 @@
+// Routes
+Router.configure({
+  layoutTemplate: 'main'
+});
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});
+Router.route('/register');
+Router.route('/login');
+Router.route('/room');
+
 /**
 * Templates
 */
