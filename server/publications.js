9fecb254b045e347aea81215894472fe93215265
Marc Tobias <TOBIASM@EMAIL.CHOP.EDU>
2016-01-24 23:56:29 -0500

    limit posts to rooms that belong to user

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,11 @@ Meteor.publish("messages", function() {
   if ( Roles.userIsInRole( this.userId, 'assistant') ) {
     return Messages.find();
   } else if ( Roles.userIsInRole( this.userId, 'patient') ) {
-    return Messages.find();
+    var roomsForCurrentUser = Rooms.find({ userIds: this.userId }).fetch().map(function(room) { 
+        return room._id;
+    });  // Gets an array of all Room IDs for the user.
+    //console.log(roomsForCurrentUser);
+    return Messages.find({ roomId: { $in: roomsForCurrentUser } });
     //return Messages.find();
   } else {
     this.stop();
